930d10ac0613383289029df33977525e49acd0c9
boyko.amarov <boyko.amarov@gmail.com>
2018-03-25 23:34:35 +0300

    Added Problem_1.R, Problem_2.R

---
+++ Problem_1.R
@@ -0,0 +1,13 @@
+
+# Homework 2
+# Problem 1
+
+# Install the following packages if necessary
+# install.packages(c('dplyr', 'ggplot2'))
+
+# Load the packages
+
+library(dplyr)
+library(ggplot2)
+
+wines <- read.csv(file='https://s3.eu-central-1.amazonaws.com/sf-timeseries/data/wine.csv')
---
+++ Problem_2.R
@@ -0,0 +1,8 @@
+# Homework 2
+# Problem 2
+
+# Install the following packages if necessary
+# install.packages(c('ggplot2', 'dplyr'))
+
+library(dplyr)
+library(ggplot2)
